e9fb79dd16ba65e991994cd9c18eae4abb9adc38
Guilherme Sousa Ribeiro <guilherme.tec1107@gmail.com>
2021-06-13 21:57:21 -0300

    class

---
+++ curso-python/desafios/t2.py
@@ -0,0 +1,4 @@
+dia = input ('que dia você nasceu? ')
+mes = input ('qual o mês do seu nascimento? ')
+ano = input ('qual o ano do seu nascimento? ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
